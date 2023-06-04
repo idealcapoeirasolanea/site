6ea1c1165a528fb667a6bc101956b4def0c1d8a1
idealcapoeirasolanea <127629660+idealcapoeirasolanea@users.noreply.github.com>
2023-06-04 09:45:54 -0300

    Update

---
+++ precache-manifest.f5e1c7ff71be8f9ff152a2a154f83cc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0768994f94508151250d",
+    "url": "/static/css/main.11b7c1e8.chunk.css"
+  },
+  {
+    "revision": "0768994f94508151250d",
+    "url": "/static/js/main.0768994f.chunk.js"
+  },
+  {
+    "revision": "715eb9b8827e76bb2389",
+    "url": "/static/css/1.1f0f1955.chunk.css"
+  },
+  {
+    "revision": "715eb9b8827e76bb2389",
+    "url": "/static/js/1.715eb9b8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b40282465676402076a5c93d313abc46",
+    "url": "/index.html"
+  }
+];
